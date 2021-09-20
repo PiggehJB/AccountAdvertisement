448650b815a1ba7cba66673d718d6200a9927d5b
PiggehJB <90732485+PiggehJB@users.noreply.github.com>
2021-09-20 16:50:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ async def on_ready():
     print("Active")
     for guild in bot.guilds:
         for channel in guild.channels:
+            # Tries to send message, if it can't (ex: no permissions, etc) it will print out that it can't and go onto the next channel
             try:
+                # If it can, it will do the following:
                 await channel.send(send_message)
                 print(f"Sent to {channel.name}")
             except:
+                # If it can't, it will do the following:
                 print(f"Couldn't send message to {channel}")
 
 
